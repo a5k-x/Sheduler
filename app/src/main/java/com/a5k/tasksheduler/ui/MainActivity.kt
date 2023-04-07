dce0e386112f7733a2ad56079890fc2c66ad55bf
Каштанов Александр Сергеевич <aleks-hd2015@yandex.ru>
2023-04-07 21:37:15 +0400

    Initial commit

---
+++ app/src/main/java/com/a5k/tasksheduler/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.a5k.tasksheduler.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.a5k.tasksheduler.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
